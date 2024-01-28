ae27cb2734231ea2de2e5c2fd0c6c108aa61a30a
Krishna <iamlearner195@gmail.com>
2024-01-28 13:49:11 -0500

    Initial Commit

---
+++ AppGenesis/AppGenesisApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppGenesisApp.swift
+//  AppGenesis
+//
+//  Created by LAKSHMI VENKATA SIVA TUNUGUNTLA on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct AppGenesisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
